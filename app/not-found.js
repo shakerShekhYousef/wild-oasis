65c794c9e10e5b143a0898e6c60147eddb01bf42
shakerShekhYousef <shakershekhyousef98@gmail.com>
2024-11-10 12:29:54 +0400

    initial commit

--- app/not-found.js
+++ app/not-found.js
@@ -6,12 +6,12 @@ function NotFound() {
         <h1 className='text-3xl font-semibold'>
           This page could not be found :(
         </h1>
-        <a
+        <Link
           href='/'
           className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg'
         >
           Go back home
-        </a>
+        </Link>
       </main>
     );
   }
